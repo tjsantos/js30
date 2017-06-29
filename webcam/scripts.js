afa2795b6e96773c85626e3c204bcd3dbd1e8f29
TJ Santos <tjscode@outlook.com>
2017-06-28 19:03:14 -0700

    webcam: draw webcam to canvas

--- webcam/scripts.js
+++ webcam/scripts.js
@@ -3,3 +3,23 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+navigator.mediaDevices
+    .getUserMedia({video: true, audio: false})
+    .then(stream => {
+        video.srcObject = stream;
+        video.play();
+        video.addEventListener(`loadedmetadata`, () => {
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            requestAnimationFrame(computeFrame);
+        });
+    })
+    .catch(err => {
+        console.log("An error occured! " + err);
+    });
+
+function computeFrame(timestamp) {
+    ctx.drawImage(video, 0, 0);
+    requestAnimationFrame(computeFrame);
+}
