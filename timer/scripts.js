c4fb6878dcfc303598e8976dcfcae743eace5e66
TJ Santos <tjscode@outlook.com>
2017-07-02 18:06:17 -0700

    timer: implemented

---
+++ timer/scripts.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const timeLeft = document.querySelector(`.display__time-left`);
+const endTime = document.querySelector(`.display__end-time`);
+
+let timeout;
+function setTimer(seconds) {
+    clearTimeout(timeout);
+    const end = new Date(Date.now() + seconds*1000);
+    const hours = end.getHours() % 12 || 12;
+    let minutes = end.getMinutes();
+    if (minutes < 10) minutes = `0` + minutes;  // left-pad `0`
+    endTime.textContent = `Be back at ${hours}:${minutes}`;
+
+    // update time left every second
+    (function tick() {
+        const diff = end.getTime() - Date.now();  // ms until time is up
+        if (diff <= 0) {
+            timeLeft.textContent = `0:00`;
+        } else {
+            let seconds = Math.round(diff / 1000);
+            let minutes = Math.floor(seconds / 60);
+            seconds = seconds % 60;
+            if (seconds < 10) seconds = `0` + seconds;  // left-pad `0`
+            timeLeft.textContent = `${minutes}:${seconds}`;
+
+            timeout = setTimeout(tick, 1000);
+        }
+    })();
+}
+
+// buttons
+document.querySelectorAll(`button.timer__button`).forEach((button) => {
+    button.addEventListener(`click`, (e) => {
+        setTimer(+button.dataset.time);
+    });
+});
+
+// form
+document.querySelector(`#custom`).addEventListener(`submit`, (e) => {
+    const form = e.currentTarget;
+    const fd = new FormData(form);
+    const minutes = +fd.get(`minutes`);
+    if (Number.isFinite(minutes) && minutes > 0) {
+        setTimer(minutes * 60);
+        form.reset();
+    }
+    e.preventDefault();
+});
+
+setTimer(10);
